365f8451cf6bb1b9b75d0a9e358922221dd0cae0
Duan Hongmei <duanhongmei@sfis-mac-pro.local>
2017-11-29 10:49:22 +0800

    第二次测试

---
+++ xcode server test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcode server test
+//
+//  Created by Duan Hongmei on 2017/11/21.
+//  Copyright © 2017年 Duan Hongmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcode server test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  xcode server test
+//
+//  Created by Duan Hongmei on 2017/11/21.
+//  Copyright © 2017年 Duan Hongmei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
